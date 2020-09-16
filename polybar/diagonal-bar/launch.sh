f0bc7e179081291f9253c07cd488f6853e775de6
Itziar Morales Rodríguez <itzimr97@outlook.com>
2020-09-16 14:21:26 +0200

    Polybar Prettified

---
+++ polybar/diagonal-bar/launch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar
+echo "---" | tee -a /tmp/polybar.log
+polybar -c ~/.config/polybar/diagonal-bar/config.ini top >>/tmp/polybar.log 2>&1 &
+
+echo "Bars launched..."
