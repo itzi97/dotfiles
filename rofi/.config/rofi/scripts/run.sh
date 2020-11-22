4bd3892b4bc0304eb94dbb85a4dc56e9758d50c3
Itziar Morales Rodríguez <itzimr97@outlook.com>
2020-11-22 19:03:03 +0100

    Rofi script modified to have high dpi option

--- rofi/.config/rofi/scripts/run.sh
+++ rofi/.config/rofi/scripts/run.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-rofi -show run -dpi 192 -run-shell-command '{terminal} -e \\" {cmd}; read -n 1 -s\\"'
+
+screen_name=$(xrandr | grep -w connected | awk -v OFS=' ' '{print $1}')
+
+if [[ "${screen_name}" = "eDP1" ]]; then
+  # HiDPI Screens
+  rofi -show run -dpi 192 \
+    -run-shell-command '{terminal} -e \\" {cmd}; read -n 1 -s\\"'
+else
+  # Normal DPI
+  rofi -show run -run-shell-command '{terminal} -e \\" {cmd}; read -n 1 -s\\"'
+fi
